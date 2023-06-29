dc3199755873b5ee78dc2ec84d81b712d6cdaa63
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-06-29 10:23:57 -0300

    135

---
+++ problems/leetcode/374-guess-number-higher-or-lower.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    int guessNumber(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (guess(mid) <= 0) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
