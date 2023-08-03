05d2f1064a4726cde847e9be84da04e0f3a1dc7f
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-08-02 21:23:24 -0300

    152

---
+++ problems/leetcode/79-word-search.cpp
@@ -0,0 +1,33 @@
+class Solution {
+   public:
+    bool dfs(vector<vector<char>>& board, int i, int j, int n, int m, string& word, int index) {
+        if (index == word.size()) {
+            return true;
+        }
+        if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] != word[index]) {
+            return false;
+        }
+        char current = board[i][j];
+        board[i][j] = '#';
+        if (dfs(board, i, j - 1, n, m, word, index + 1) ||
+            dfs(board, i, j + 1, n, m, word, index + 1) ||
+            dfs(board, i - 1, j, n, m, word, index + 1) ||
+            dfs(board, i + 1, j, n, m, word, index + 1)) {
+            return true;
+        }
+        board[i][j] = current;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size(), m = board[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (dfs(board, i, j, n, m, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
