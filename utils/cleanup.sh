e7c8891ef992ab3c8ad675bf1f304824e3e35dd0
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-09-03 19:18:24 -0300

    chore: add utils folder

--- utils/cleanup.sh
+++ utils/cleanup.sh
@@ -1,6 +1,6 @@
 # ~/.zshrc
-# alias c="code ~/Projects/cp && exit"
-# alias f="cd ~/Projects/cp && source cleanup.sh && find problems -type f | wc -l"
+# alias c="code ~/projects/cp && exit"
+# alias f="cd ~/projects/cp && source utils/cleanup.sh && find problems -type f | wc -l"
 cd problems
 find . -type f -not \( -name '*.c' -or -name '*.cpp' -or -name '*.py' -or -name '*.sql' -or -name '*.exs' -or -name '*.txt' -or -name '*.sh' -or -name '*.md' \) -delete
 find . -type f \( -name '1.py' \) -delete
